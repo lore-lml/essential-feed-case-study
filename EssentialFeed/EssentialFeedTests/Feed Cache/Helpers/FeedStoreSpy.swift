db1e9b7413ae4209fd8a239f13b2594aa450250d
Lorenzo Limoli <l.limoli@reply.it>
2022-10-26 11:04:06 +0200

    Load command fails on retrieval error

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -21,6 +21,7 @@ class FeedStoreSpy: FeedStore{
     
     private var deletionCompletions = [DeletionCompletion]()
     private var insertionCompletions = [InsertionCompletion]()
+    private var retrievalCompletions = [RetrievalCompletion]()
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion){
         deletionCompletions.append(completion)
@@ -43,12 +44,17 @@ class FeedStoreSpy: FeedStore{
         insertionCompletions[index](nil)
     }
     
+    func completeRetrieval(with error: Error, at index: Int = 0){
+        retrievalCompletions[index](error)
+    }
+    
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion){
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(feed, timestamp))
     }
     
-    func retrieve(){
+    func retrieve(completion: @escaping RetrievalCompletion){
+        retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -19,15 +19,34 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
     func test_load_requestsCacheRetrieval(){
         let (sut, store) = makeSUT()
         
-        sut.load()
+        sut.load(){ _ in }
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failsOnRetrievalError(){
+        let (sut, store) = makeSUT()
+        let retrievalError = anyNSError
+        let exp = expectation(description: "Waiting for load completion")
+        
+        var receivedError: Error?
+        sut.load{ error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as? NSError, retrievalError)
     }
 }
 
 
 // MARK: Helpers
 extension LoadFeedFromCacheUseCaseTests{
+    
+    private var anyNSError: NSError{ .init(domain: "any error", code: 1) }
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy){
         let store = FeedStoreSpy()
