72851978684d7cafaf8ec34174dcef995456e774
Lorenzo Limoli <l.limoli@reply.it>
2022-10-28 17:28:48 +0200

    Delete delivers error on deletion error (URL with no delete permission)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -84,9 +84,12 @@ class CodableFeedStore{
             return
         }
         
-        try! FileManager.default.removeItem(at: storeURL)
-        
-        completion(nil)
+        do{
+            try FileManager.default.removeItem(at: storeURL)
+            completion(nil)
+        }catch{
+            completion(error)
+        }
     }
 }
 
@@ -200,12 +203,26 @@ final class CodableFeedStoreTests: XCTestCase {
         
         expect(sut, toRetrieve: .empty)
     }
+    
+    func test_delete_deliversErrorOnDeletionError(){
+        let noDeletePermissionURL = cachesDirectory
+        let sut = makeSUT(storeURL: noDeletePermissionURL)
+        
+        let deletionError = deleteCache(from: sut)
+        
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
+        expect(sut, toRetrieve: .empty)
+    }
 }
 
 
 private extension CodableFeedStoreTests{
     
-    private var testSpecificStoreURL: URL{ FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store") }
+    private var testSpecificStoreURL: URL{ cachesDirectory.appendingPathComponent("\(type(of: self)).store") }
+    
+    private var cachesDirectory: URL{
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore{
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL)
