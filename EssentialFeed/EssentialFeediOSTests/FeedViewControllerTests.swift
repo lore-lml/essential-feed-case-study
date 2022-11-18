3a0d1842db654a02e85065e403e08ac2e34c6dbc
Lorenzo Limoli <l.limoli@reply.it>
2022-11-18 15:16:58 +0100

    Changed `UIImage.make` to fix issue for iOS 16.1. Now it creates an image with a fixed scale in order to being able to compare image data pixel by pixel

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -426,12 +426,12 @@ private extension FeedImageCell{
 private extension UIImage {
     static func make(withColor color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { context in
+            color.setFill()
+            context.fill(rect)
+        }
     }
 }
