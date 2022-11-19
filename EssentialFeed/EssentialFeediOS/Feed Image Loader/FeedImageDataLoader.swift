46c12a262ebf4b45cf48f502b5cd1b862bdddca1
Lorenzo Limoli <l.limoli@reply.it>
2022-11-19 16:54:42 +0100

    Group files in an MVC folder structure

---
+++ EssentialFeed/EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Lorenzo Limoli on 19/11/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask{
+    func cancel()
+}
+
+public protocol FeedImageDataLoader{
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
