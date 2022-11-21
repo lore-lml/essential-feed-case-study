a403acaa36029fb07475d0b4bc7e07900c81d8ba
Lorenzo Limoli <l.limoli@reply.it>
2022-11-21 13:43:17 +0100

    Move View Models to their own files

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Lorenzo Limoli on 21/11/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel{
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Lorenzo Limoli on 21/11/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+
+struct FeedViewModel{
+    let feed: [FeedImage]
+}
