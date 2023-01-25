78f0e28347462b316618cbec9bd43909e5d532b1
Lorenzo Limoli <l.limoli@reply.it>
2023-01-25 20:05:11 +0100

    Empty Essential App Project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Lorenzo Limoli on 25/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
